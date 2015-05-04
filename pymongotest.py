671d568a8908624893ba7c4e9a7ce986cdd9a91b
shakingWaves <shakingWaves@hotmail.com>
2015-05-04 18:36:39 +0800

    pymongotest

--- pymongotest.py
+++ pymongotest.py
@@ -1,27 +1,53 @@
 #!/usr/bin/env python
+import re
 import pymongo
 #connection
-conn=pymongo.Connection("127.0.0.1",2201)
-db=conn.test
+def connect():
+    conn=pymongo.Connection("127.0.0.1",2201)
+    db=conn.test
+    return db
 #insert
-u=dict(name="lixiaobo",age=23)
-db.lixb.save(u)
-v=dict(name="2222",age=24)
-db.lixb.save(v)
-for i in range(10):
-	db.lixb.save({"name":"li"+str(i),"age":i+10})
-result=db.lixb.find()
-u=dict(name="listtt",age=25)
-db.lixb.insert(u)
-print list(result),
+def insertOp(db):
+    u=dict(name="l2ixiaobo",age=23)
+    db.lixb.save(u)
+    for i in range(10):
+        db.lixb.save({"name":"li"+str(i),"age":i+10})
+    u=dict(name="l3isttt",age=25)
+    db.lixb.insert(u)
 #update single
-u2=db.lixb.find_one({"name":"listtt"})
-if u2 and hasattr(u2,"age"):
-	u2['age']+=3
-else：
-	print "No such attribute"
-db.lixb.save(u2)
-#update multiply records
-db.lixb.update({},{"$inc":{"age":10}},nulti=True)
-db.lixb.update({"name":"lixiaobo"},{"$inc":{"age":10},"$set":{"sex":1}})
+def updateOp(db):
+    u2=db.lixb.find_one({"name":"l3isttt"})
+    if u2 and hasattr(u2,"age"):
+        u2['age']+=3
+        db.lixb.save(u2)
+    elif u2:
+        db.lixb.update({"name":"l3isttt"},{"name":"l3isttt","age":25,"sex":2})
+        print  db.lixb.find_one({"name":"l3isttt"})
+    #update multiply records
+    db.lixb.update({},{"$inc":{"age":10}},multi=True)
+    print "before update:" ,list(db.lixb.find({"name":"l2ixiaobo"}))
+    db.lixb.update({"name":"l2ixiaobo"},{"$inc":{"age":10},"$set":{"sex":1}})
+    print "after update:" ,list(db.lixb.find({"name":"l2ixiaobo"}))
+    #search
+    result=db.lixb.find()
+    for i in result: 
+        print  i
+#delete
+def deleteOp(db):
+    #regx=re.compile('^2*')
+    #result=db.lixb.find({'name':regx})
+    result=db.lixb.find({'name':{'$regex':'l*'}})
+    print "after delete:"
+    for i in result: 
+        print  i
+        db.lixb.remove(i)
+    
+    for i in result: 
+        print  i
 
+if __name__=='__main__':
+    db=connect()
+    db.lixb.drop()
+    insertOp(db)
+    updateOp(db)
+    deleteOp(db)
